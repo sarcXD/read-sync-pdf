561182942495a2e9f831ad038b0cac630ebd800a
talha aamir <talhaaamir@ricult.com>
2021-05-10 04:33:49 +0500

    Setup:
    * pdf viewer
    * page count
    * interaction and pdf fetching from backend

---
+++ src/components/pdfViewer/textLayer.js
@@ -0,0 +1,49 @@
+const SEARCH_FOR = ""; // try 'Mozilla';
+
+const container = document.getElementById("viewerContainer");
+
+const eventBus = new pdfjsViewer.EventBus();
+
+// (Optionally) enable hyperlinks within PDF files.
+const pdfLinkService = new pdfjsViewer.PDFLinkService({
+  eventBus,
+});
+
+// (Optionally) enable find controller.
+const pdfFindController = new pdfjsViewer.PDFFindController({
+  eventBus,
+  linkService: pdfLinkService,
+});
+
+const pdfViewer = new pdfjsViewer.PDFViewer({
+  container,
+  eventBus,
+  linkService: pdfLinkService,
+  findController: pdfFindController,
+});
+pdfLinkService.setViewer(pdfViewer);
+pdfScriptingManager.setViewer(pdfViewer);
+
+eventBus.on("pagesinit", function () {
+  // We can use pdfViewer now, e.g. let's change default scale.
+  pdfViewer.currentScaleValue = "page-width";
+
+  // We can try searching for things.
+  if (SEARCH_FOR) {
+    pdfFindController.executeCommand("find", { query: SEARCH_FOR });
+  }
+});
+
+// Loading document.
+const loadingTask = pdfjsLib.getDocument({
+  url: DEFAULT_URL,
+  cMapUrl: CMAP_URL,
+  cMapPacked: CMAP_PACKED,
+});
+loadingTask.promise.then(function (pdfDocument) {
+  // Document loaded, specifying document for the viewer and
+  // the (optional) linkService.
+  pdfViewer.setDocument(pdfDocument);
+
+  pdfLinkService.setDocument(pdfDocument, null);
+});
