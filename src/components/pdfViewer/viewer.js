52add7d9dfa0dd4bc410b61704bab50a563cf639
talha aamir <talhaaamir@ricult.com>
2021-07-28 01:51:26 +0500

    ADDED: Basic toolbar functionality added, File upload to firebase created

--- src/components/pdfViewer/viewer.js
+++ src/components/pdfViewer/viewer.js
@@ -1,6 +1,7 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { GetData } from "../services/api.service.js"; // curly braces since no default export in file
-
+import "./viewer.css";
+import { storage } from "./../../config";
 const pdfjsLib = window.pdfjsLib;
 const pdfjsViewer = window.pdfjsViewer;
 
@@ -11,18 +12,21 @@ let containerStyle = {
   height: "100%",
 };
 
-export const pdfVariables = {
-  eventBus: null,
-  pdfDocument: null,
-  pdfViewer: null,
-  pdfLinkService: null,
-  pdfFindController: null,
-  pdfScriptingManager: null,
-}
+// const pdfVariables = {
+// eventBus: null,
+// pdfDocument: null,
+// pdfViewer: null,
+// pdfLinkService: null,
+// pdfFindController: null,
+// pdfScriptingManager: null,
+// }
 
 function Viewer() {
   const containerRef = useRef(null);
   const viewerRef = useRef(null);
+  const [pdfVariables, setPdfVariables] = useState({});
+  const [pgNum, setPgNum] = useState(undefined);
+  const [openFile, setOpenFile] = useState(null);
 
   // The workerSrc property shall be specified.
   pdfjsLib.GlobalWorkerOptions.workerSrc =
@@ -42,16 +46,16 @@ function Viewer() {
     renderInitialPdf(resp);
   };
 
-  const handlePageInit = (
-    SEARCH_FOR
-  ) => {
+  const handlePageInit = (SEARCH_FOR) => {
     pdfVariables.eventBus.on("pagesinit", function () {
       // We can use pdfViewer now, e.g. let's change default scale.
       pdfVariables.pdfViewer.currentScaleValue = "page-width";
       pdfVariables.pdfViewer.currentPageNumber = 5;
       // We can try searching for things.
       if (SEARCH_FOR) {
-        pdfVariables.pdfFindController.executeCommand("find", { query: SEARCH_FOR });
+        pdfVariables.pdfFindController.executeCommand("find", {
+          query: SEARCH_FOR,
+        });
       }
     });
   };
@@ -104,7 +108,6 @@ function Viewer() {
     pdfVariables.pdfScriptingManager = pdfScriptingManager;
     pdfVariables.pdfLinkService = pdfLinkService;
     pdfVariables.pdfFindController = pdfFindController;
-    
 
     handlePageInit(SEARCH_FOR);
     // Loading document.
@@ -113,9 +116,41 @@ function Viewer() {
       cMapUrl: CMAP_URL,
       cMapPacked: CMAP_PACKED,
     });
-    
+
     await handleDocLoad(loadingTask);
-    console.log(pdfVariables.pdfDocument.numPages);
+  };
+
+  const gotoPage = ($ev) => {
+    console.log($ev);
+    if ($ev.keyCode === 13) {
+      $ev.preventDefault();
+      console.log(pgNum);
+      pdfVariables.pdfViewer.currentPageNumber = pgNum;
+    }
+  };
+
+  const setPage = ($ev) => {
+    console.log(2);
+    const intVal = parseInt($ev.target.value);
+    setPgNum(intVal);
+  };
+
+  const uploadToFirebase = (file) => {
+    if (file) {
+      console.log(file)
+      const storageRef = storage.ref();
+      const pdfRef = storageRef.child(file.name);
+      pdfRef.put(file).then(() => {
+        alert("File uploaded successfully");
+      });
+    }
+  };
+
+  const uploadAndOpenPdf = ($ev) => {
+    const file = $ev.target.files[0];
+    setOpenFile(file);
+    uploadToFirebase(file);
+    // downloadAndOpenFile();
   };
 
   return (
@@ -127,9 +162,33 @@ function Viewer() {
       >
         <div id="#viewer" className="pdfViewer" ref={viewerRef} />
       </div>
-      <button onClick={loadPdf} style={{ position: "absolute" }}>
-        Load PDF from backend
-      </button>
+      <div className="toolbar-ctn">
+        <div className="start-ctn">
+          <input
+            className="upload-btn"
+            type="file"
+            accept=".pdf"
+            onChange={uploadAndOpenPdf}
+          />
+        </div>
+        <div className="mid-ctn">
+          <button onClick={loadPdf} className="toolbar-btn">
+            Load PDF from backend
+          </button>
+          <input
+            type="number"
+            value={pgNum}
+            onChange={setPage}
+            onKeyDown={gotoPage}
+            className="toolbar-input"
+          />
+          <div className="pg-text">of</div>
+          <div id="total-pages">{pdfVariables?.pdfDocument?.numPages}</div>
+        </div>
+        <div className="end-ctn">
+          <button>Save</button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,17 @@
+import {firebase} from "@firebase/app";
+import "@firebase/storage";
+
+console.log(process.env)
+const app = firebase.initializeApp({
+    apiKey: "AIzaSyC28NIuL_06bJDdK2454mRYN4d5fyqgn_k",
+    authDomain: "readsyncpdf.firebaseapp.com",
+    projectId: "readsyncpdf",
+    storageBucket: "readsyncpdf.appspot.com",
+    messagingSenderId: "1030733509987",
+    appId: "1:1030733509987:web:869436f3e53258482b8f56",
+    measurementId: "G-835RS0WS5S",
+})
+
+export const storage = firebase.storage();
+
+export default app;
