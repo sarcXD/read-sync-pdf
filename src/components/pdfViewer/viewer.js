53ee7a6dd73ca3b978cd6482989dce9b447d0f96
talha aamir <talhaaamir@ricult.com>
2021-08-05 22:39:58 +0500

    Setup pdf loading from firebase storage Setup google login Setup google logout Setup state persistance check for if user logged in?

--- src/components/pdfViewer/viewer.js
+++ src/components/pdfViewer/viewer.js
@@ -1,55 +1,59 @@
-import { useRef, useState, useEffect } from 'react';
-import { PostData } from '../services/api.service.js'; // curly braces since no default export in file
-import './viewer.css';
-import { storage } from './../../config';
+import { useRef, useState, useEffect } from "react";
+import { PostData } from "../services/api.service.js"; // curly braces since no default export in file
+import "./viewer.css";
+import { storage, firebaseRef } from "./../../config";
+
 const pdfjsLib = window.pdfjsLib;
 const pdfjsViewer = window.pdfjsViewer;
 
 let containerStyle = {
-  overflow: 'auto',
-  position: 'absolute',
-  width: '100%',
-  height: '100%',
+  overflow: "auto",
+  position: "absolute",
+  width: "100%",
+  height: "100%",
 };
 
 function Viewer() {
   const containerRef = useRef(null);
   const viewerRef = useRef(null);
   const [pdfVariables, setPdfVariables] = useState({});
-  const [pgNum, setPgNum] = useState('');
+  const [pgNum, setPgNum] = useState("");
   const [openFile, setOpenFile] = useState(null);
-  const [blobData, setblobData] = useState('');
+  const [signedIn, setSignedIn] = useState(false);
+  const [userState, setUserState] = useState({});
 
   // The workerSrc property shall be specified.
   pdfjsLib.GlobalWorkerOptions.workerSrc =
-    'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.8.335/pdf.worker.min.js';
+    "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.8.335/pdf.worker.min.js";
 
   // Some PDFs need external cmaps.
   //
-  const CMAP_URL = '../../node_modules/pdfjs-dist/cmaps/';
+  const CMAP_URL = "../../node_modules/pdfjs-dist/cmaps/";
   const CMAP_PACKED = true;
 
   // sandbox link
   const SANDBOX_BUNDLE_SRC =
-    'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.8.335/pdf.sandbox.min.js';
+    "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.8.335/pdf.sandbox.min.js";
 
   const loadPdf = async () => {
+    // const file = await downloadFileFromFB();
     const url = await downloadFileFromFB();
     if (url) {
-      console.log('URL', url);
-      const resp = await PostData('http://localhost:8000/pdf', { dldUrl: url });
-      renderInitialPdf({data: atob(resp)});
+      // console.log('URL', url);
+      // const resp = await PostData('http://localhost:8000/pdf', { dldUrl: url });
+      // console.log(resp)
+      renderInitialPdf(url);
     }
   };
 
   const handlePageInit = (SEARCH_FOR, tempStateVar) => {
-    tempStateVar.eventBus.on('pagesinit', function () {
+    tempStateVar.eventBus.on("pagesinit", function () {
       // We can use pdfViewer now, e.g. let's change default scale.
 
-      tempStateVar.pdfViewer.currentScaleValue = 'page-width';
+      tempStateVar.pdfViewer.currentScaleValue = "page-width";
       // We can try searching for things.
       if (SEARCH_FOR) {
-        tempStateVar.pdfFindController.executeCommand('find', {
+        tempStateVar.pdfFindController.executeCommand("find", {
           query: SEARCH_FOR,
         });
       }
@@ -69,7 +73,7 @@ function Viewer() {
     if (containerRef == null || containerRef.current == null) {
       return;
     }
-    const SEARCH_FOR = 'starters'; // try 'Mozilla';
+    const SEARCH_FOR = "starters"; // try 'Mozilla';
     const container = containerRef.current;
     const eventBus = new pdfjsViewer.EventBus();
     // (Optionally) enable hyperlinks within PDF files.
@@ -109,7 +113,7 @@ function Viewer() {
     handlePageInit(SEARCH_FOR, tempStateVar);
     // Loading document.
     const loadingTask = pdfjsLib.getDocument({
-      data: rawPDFUrl.data,
+      url: rawPDFUrl,
       cMapUrl: CMAP_URL,
       cMapPacked: CMAP_PACKED,
     });
@@ -137,7 +141,7 @@ function Viewer() {
       const storageRef = storage.ref();
       const pdfRef = storageRef.child(file.name);
       pdfRef.put(file).then(() => {
-        alert('File uploaded successfully');
+        alert("File uploaded successfully");
       });
     }
   };
@@ -146,7 +150,6 @@ function Viewer() {
     const storageRef = storage.ref();
     const refs = await storageRef.listAll();
     const url = await refs.items[0].getDownloadURL();
-    // downloadBlobFile(url)
     return url;
   };
 
@@ -156,7 +159,55 @@ function Viewer() {
     uploadToFirebase(file);
   };
 
+  const signInGoogle = () => {
+    const provider = new firebaseRef.auth.GoogleAuthProvider();
+    firebaseRef
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        var credential = result.credential;
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        setSignedIn(true);
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+      });
+  };
+
+  const logoutGoogle = () => {
+    firebaseRef.auth().signOut().then(() => {
+      // saveUserState()
+      setSignedIn(false);
+    }).catch((error) => {
+      // Error occurred
+    })
+  }
+
+  // Performed when user logs in to the page
+  // Performed when user logs out
+  const authCheck = () => {
+    firebaseRef.auth().onAuthStateChanged((user)=> {
+      if (user) {
+        const uid = user.uid;
+        setSignedIn(true);
+      } else {
+        setSignedIn(false);
+      }
+    })
+  }
+
   useEffect(() => {
+    authCheck();
     loadPdf();
   }, []);
 
@@ -169,33 +220,44 @@ function Viewer() {
       >
         <div id="#viewer" className="pdfViewer" ref={viewerRef} />
       </div>
-      <div className="toolbar-ctn">
-        <div className="start-ctn">
-          <input
-            className="upload-btn"
-            type="file"
-            accept=".pdf"
-            onChange={uploadAndOpenPdf}
-          />
+      {signedIn ? (
+        <div className="toolbar-ctn">
+          <div className="start-ctn">
+            <input
+              className="upload-btn"
+              type="file"
+              accept=".pdf"
+              onChange={uploadAndOpenPdf}
+            />
+          </div>
+          <div className="mid-ctn">
+            <button onClick={loadPdf} className="toolbar-btn">
+              Load PDF from backend
+            </button>
+            <input
+              type="number"
+              value={pgNum}
+              onChange={setPage}
+              onKeyDown={gotoPage}
+              className="toolbar-input"
+            />
+            <div className="pg-text">of</div>
+            <div id="total-pages">{pdfVariables?.pdfDocument?.numPages}</div>
+          </div>
+          <div className="end-ctn">
+            <button>Save</button>
+            <button onClick={logoutGoogle} className="logout-btn">Logout</button>
+          </div>
         </div>
-        <div className="mid-ctn">
-          <button onClick={loadPdf} className="toolbar-btn">
-            Load PDF from backend
-          </button>
-          <input
-            type="number"
-            value={pgNum}
-            onChange={setPage}
-            onKeyDown={gotoPage}
-            className="toolbar-input"
-          />
-          <div className="pg-text">of</div>
-          <div id="total-pages">{pdfVariables?.pdfDocument?.numPages}</div>
+      ) : (
+        <div className="toolbar-ctn">
+          <div className="end-ctn">
+            <button onClick={signInGoogle} className="sign-in-btn">
+              Sign In
+            </button>
+          </div>
         </div>
-        <div className="end-ctn">
-          <button>Save</button>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import {firebase} from "@firebase/app";
 import "@firebase/storage";
-
+import "@firebase/auth";
 const app = firebase.initializeApp({
     apiKey: "AIzaSyC28NIuL_06bJDdK2454mRYN4d5fyqgn_k",
     authDomain: "readsyncpdf.firebaseapp.com",
@@ -12,5 +12,7 @@ const app = firebase.initializeApp({
 })
 
 export const storage = firebase.storage();
+export const firebaseRef = firebase;
+export const auth = firebase.auth;
 
 export default app;
